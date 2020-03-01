8a331b4172fbaaf744ea1b7277635f21b13f21c6
U2 <zhyale@gmail.com>
2020-03-01 19:41:59 +0800

    i18n test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
-import { HttpModule }    from '@angular/http';
+//import { HttpModule }    from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
@@ -138,7 +138,7 @@ export class MaterialModule{}
     MaterialModule,
     BrowserModule, 
     BrowserAnimationsModule,
-    HttpModule,
+    //HttpModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule
