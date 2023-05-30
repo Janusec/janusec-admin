fcac35f4a13eefb01e2dcdb5490aee4ebe412445
zhyale <zhyale@gmail.com>
2023-05-30 23:18:47 +0800

    add tab for application cookie management

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -120,7 +120,7 @@ let zhTranslations = {
     // Application Detail
     'app_detail': '应用配置',
     'app_name': '应用名称',
-    'app_internal_scheme': '后端/内部Scheme（网关访问后端使用HTTP还是HTTPS，默认HTTP）',
+    'app_internal_scheme': '内部Scheme（网关访问源服务器，默认HTTP）',
     'app_destination': '目标地址及端口（如：10.10.10.10:80 或者 [::1%lo]:8080 ）',
     'add_new_routing': '添加新的路由',
     'domain_name': '域名（必填）',
@@ -151,7 +151,9 @@ let zhTranslations = {
     'routing_type_tooltip': '默认Reverse_Proxy（反向代理）, Local_FastCGI 用于PHP/Python等, Static_Website 用于本地静态网站(无后端服务器)，K8S_Ingress用于K8S Pods反向代理',
     'destination_tooltip': '默认 IP:Port （如10.0.0.1:80）, 用于静态网站时填写默认文件名（如index.html）',
     'backend_routing_tooltip': '默认 / , 前后端路径不一致时可使用 /xyz/ 这种形式，本地静态网站或FastCGI网站请填写绝对路径 /path/to/www/ ',
-    'cookie_mgmt_enabled': '启用Cookie Banner (ePrivacy/GDPR合规)',
+    'cookie_mgmt_enabled': '启用Cookie合规管理',
+    'basic_information': '应用基本信息',
+    'cookie_management': '应用Cookie管理',
     // Port forwarding
     'port_forwarding': '端口转发',
     'vip_note': '注：本模块为四层TCP/UDP端口转发（无WAF/CC防护功能）。',
