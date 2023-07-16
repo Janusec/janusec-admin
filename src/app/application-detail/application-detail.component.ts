002b5972426bdb7fccac4c6c9121b024f2aa4fe0
zhyale <zhyale@gmail.com>
2023-07-16 19:02:30 +0800

    fix add new application bug due to null custom headers

--- src/app/application-detail/application-detail.component.ts
+++ src/app/application-detail/application-detail.component.ts
@@ -65,6 +65,7 @@ export class ApplicationDetailComponent implements OnInit {
       this.application.csp = "";
       this.application.cache_enabled = true;
       this.application.description = "Used for ...";
+      this.application.custom_headers = [];
       this.addDomain();
       this.addDestination();
     }
